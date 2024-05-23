370a3c914d0cba7b7462cbb34d0148fe94e21ede
DAMI <140088921+McDamian12@users.noreply.github.com>
2024-05-22 20:09:32 -0500

    DART

---
+++ DART/listasejemplo.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+ Map Insumos={
+  "ID": "7004",
+  "Descripcion":"Oreo",
+  "Precio":"\$1200",
+  "Estado":true,
+  true:false
+ }
+ ;
+
+  Map ventas={
+  "ID": "7004",
+  "Fecha":"1 Nov, 10:20 AM",
+  "Cliente":"rodolfo emilio acevedo cano de la cruz",
+  "Estado":true,
+  "metodo-pago":"transferencia",
+  "pago":"12,000"
+ };
+ print(Insumos);
+ print(ventas);
+}
+
